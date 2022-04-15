c365c08981dba4a39a79151d5b735730d243a737
VijayMTech <82018382+VijayMTech@users.noreply.github.com>
2022-04-15 21:05:48 +0530

    Create Product Of Two.cpp

---
+++ Product Of Two.cpp
@@ -0,0 +1,13 @@
+//C++ Program to find the product of two numbers entered by the User
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float num1,num2;
+    cout<<"Enter two numbers:";
+    cin>>num1 >>num2;
+    float mul=num1*num2;
+    cout<<"Product="<<mul;
+    return 0;
+}
